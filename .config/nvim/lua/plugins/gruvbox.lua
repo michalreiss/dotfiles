ccbb93511ebf47a4fe9eed1bf3c688330af98d6d
Michał Reiss <michalreiss@hotmail.com>
2024-02-20 12:56:41 +0100

    enabled gruvbox for telescope

--- .config/nvim/lua/plugins/gruvbox.lua
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -1,7 +1,8 @@
 return {
-	"luisiacc/gruvbox-baby",
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme("gruvbox-baby")
-	end,
+    "luisiacc/gruvbox-baby",
+    priority = 1000,
+    config = function()
+        vim.g.gruvbox_baby_telescope_theme = 1
+        vim.cmd.colorscheme("gruvbox-baby")
+    end,
 }
