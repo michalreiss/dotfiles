2d8e7d27710ba8161ea6ab6d1bb997c2f2df6c6b
Michał Reiss <michalreiss@hotmail.com>
2024-03-12 11:33:10 +0100

    relative numbers enabled, description for few keybindings

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,13 +5,13 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-			vim.keymap.set("n", "<C-f>", builtin.current_buffer_fuzzy_find, {})
-			vim.keymap.set("n", "<leader>f", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>d", builtin.diagnostics, {})
-			vim.keymap.set("n", "<leader>gr", builtin.lsp_references, {})
-            vim.keymap.set("n", "<leader>gd", builtin.lsp_definitions, {})
-            vim.keymap.set("n", "<leader>gi", builtin.lsp_implementations, {})
+			vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files" })
+			vim.keymap.set("n", "<C-f>", builtin.current_buffer_fuzzy_find, { desc = "Search in current file" })
+			vim.keymap.set("n", "<leader>f", builtin.live_grep, { desc = "Search in working directory" })
+			vim.keymap.set("n", "<leader>d", builtin.diagnostics, { desc = "Diagnostics" })
+			vim.keymap.set("n", "<leader>gr", builtin.lsp_references, { desc = "Go to references" })
+			vim.keymap.set("n", "<leader>gd", builtin.lsp_definitions, { desc = "Go to definition" })
+			vim.keymap.set("n", "<leader>gi", builtin.lsp_implementations, { desc = "Go to implementation" })
 		end,
 	},
 	{
--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -10,5 +11,5 @@ vim.opt.foldlevelstart = 99
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 -- global keymaps
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move down a selection" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move up a selection" })
