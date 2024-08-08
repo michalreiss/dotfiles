703d5cf7090b6a7bddfef9540f8a5cac1548f3f8
Michał Reiss <michalreiss@hotmail.com>
2024-08-08 09:23:33 +0200

    removed highlight feature

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -57,26 +57,6 @@ return {
             vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
             vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, {})
 
-            vim.api.nvim_create_autocmd("LspAttach", {
-                callback = function(event)
-                    --  This function gets run when an LSP attaches to a particular buffer.
-                    --    That is to say, every time a new file is opened that is associated with
-                    --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-                    --    function will be executed to configure the current buffer
-                    local client = vim.lsp.get_client_by_id(event.data.client_id)
-                    if client and client.server_capabilities.documentHighlightProvider then
-                        vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-                            buffer = event.buf,
-                            callback = vim.lsp.buf.document_highlight,
-                        })
-
-                        vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-                            buffer = event.buf,
-                            callback = vim.lsp.buf.clear_references,
-                        })
-                    end
-                end,
-            })
             vim.filetype.add({ extension = { templ = "templ" } })
         end,
     },
