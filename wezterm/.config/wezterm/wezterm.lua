590fd903153dab721429571e507f7381bac12310
Michał Reiss <michalreiss@hotmail.com>
2024-08-30 09:36:22 +0200

    wezterm without window decorations

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,16 +1,17 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Rosé Pine (Gogh)'
+config.color_scheme = "Rosé Pine (Gogh)"
 config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" })
 config.font_size = 16.0
 config.enable_scroll_bar = false
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
+config.window_decorations = "RESIZE"
 config.window_padding = {
-	left = 5,
-	right = 5,
-	top = 0,
+	left = 10,
+	right = 10,
+	top = 10,
 	bottom = 0,
 }
 
