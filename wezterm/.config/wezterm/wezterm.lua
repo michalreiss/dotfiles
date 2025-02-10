f12992f62ecca15e754dabfb8ce7807f2df2a8bf
Michał Reiss <michalreiss@hotmail.com>
2025-02-10 09:32:27 +0100

    get back to wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "Rosé Pine (Gogh)"
+config.color_scheme = "tokyonight"
 config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" })
 config.font_size = 16.0
 config.enable_scroll_bar = false
@@ -9,10 +9,10 @@ config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.window_decorations = "RESIZE"
 config.window_padding = {
-	left = 10,
-	right = 10,
-	top = 10,
-	bottom = 0,
+    left = 10,
+    right = 10,
+    top = 10,
+    bottom = 0,
 }
 
 return config
