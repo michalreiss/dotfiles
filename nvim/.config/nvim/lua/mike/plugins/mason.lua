2e2edfd87135b3da6fc628f1fe8639ef9b0f239d
Michal Reiss <michalreiss@hotmail.com>
2025-12-09 00:00:31 +0100

    fix: mason ensure installed

--- nvim/.config/nvim/lua/mike/plugins/mason.lua
+++ nvim/.config/nvim/lua/mike/plugins/mason.lua
@@ -1,17 +1,26 @@
 return {
-	"williamboman/mason.nvim",
-	tag = "stable",
-	lazy = false,
-	config = true,
-	ensure_installed = {
-		"goimports-reviser",
-		"gopls",
-		"html-lsp",
-		"htmx-lsp",
-		"lua-language-server",
-		"python-lsp-server",
-		"staticcheck",
-		"templ",
-		"terraform-ls",
-	}
+	{
+		"williamboman/mason.nvim",
+		tag = "stable",
+		lazy = false,
+		config = true,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"goimports-reviser",
+					"gopls",
+					"html-lsp",
+					"htmx-lsp",
+					"lua-language-server",
+					"python-lsp-server",
+					"staticcheck",
+					"templ",
+					"terraform-ls",
+				}
+			})
+		end
+	},
 }
