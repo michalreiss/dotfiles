6a510ed59382fbaa3ab457d055d9b241aede4018
Michal Reiss <michalreiss@hotmail.com>
2026-02-08 14:13:29 +0100

    arduino fix

--- nvim/.config/nvim/lsp/arduino.lua
+++ nvim/.config/nvim/lsp/arduino.lua
@@ -10,12 +10,9 @@ return {
 	cmd = {
 		"arduino-language-server",
 		"-cli-config", vim.fn.expand("~/.arduino15/arduino-cli.yaml"),
-		"-fqbn", "esp8266:esp8266:generic",
 		"-cli", "arduino-cli",
 	},
-
 	filetypes = { "arduino" },
-
 	root_dir = function(_, on_dir)
 		on_dir(vim.fn.expand("%:p:h"))
 	end,
